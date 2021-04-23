fbb00671755757ea494864adebc4c7adb94fcdb2
zhouyang <zhouyang123200@hotmail.com>
2021-04-23 13:55:52 +0800

    fix number bug

--- static/src/js/field_widget.js
+++ static/src/js/field_widget.js
@@ -1,12 +1,12 @@
 odoo.define('china_finance_capital.num_capital', function (require) {
     "use strict";
 
-    var AbstractField = require('web.AbstractField');
+    var basic_fields = require('web.basic_fields');
     var fieldRegistry = require('web.field_registry');
     var core = require('web.core');
     var qweb = core.qweb;
 
-    var numCapitalField = AbstractField.extend({
+    var numCapitalField = basic_fields.InputField.extend({
         className: 'num_capital',
         tagName: 'span',
         supportedFieldTypes: ['integer'],
@@ -15,22 +15,22 @@ odoo.define('china_finance_capital.num_capital', function (require) {
             this._super.apply(this, arguments);
         },
 
-        _renderEdit: function () {
-            this.$el.empty();
-            this.$el = $("<input type='text'/>");
-            this.$el.addClass('o_input');
-            this.$el.val(this.value);
-        },
+        // _renderEdit: function () {
+        //     this.$el.empty();
+        //     this.$el = $("<input type='text'/>");
+        //     this.$el.addClass('o_input');
+        //     this.$el.val(this.value);
+        // },
 
         _renderReadonly: function () {
             this.$el.empty();
             var table = qweb.render('FieldNumCapital', {widget: this});
             var nums = this.value.toString();
 
-            var tr = $('<tr><tr>');
+            var tr = $('<tr></tr>');
             for (var i=9 - nums.length; i > 0; i--)
                 tr.append($('<td></td>'));
-            for (var i=nums.length - 1; i >= 0; i--)
+            for (var i=0; i <= nums.length - 1; i++)
                 tr.append($('<td></td>').text(nums[i]));
 
             table = $(table).append(tr);
@@ -40,7 +40,8 @@ odoo.define('china_finance_capital.num_capital', function (require) {
 
         _getValue: function () {
             return this.$el.val();
-        },
+        }
+
     })
 
     fieldRegistry.add('num_capital', numCapitalField);
